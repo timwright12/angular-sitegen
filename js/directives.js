32c2ae13e05be510a52d5a83edf11cf2bf57d498
Geordie Kaytes <geordie.kaytes@freshtilledsoil.com>
2014-02-09 21:56:28 -0500

    added directives comment stubs

--- js/directives.js
+++ js/directives.js
@@ -1,3 +1,24 @@
 'use strict';
 
 /* Directives */
+
+var siteGenDirectives = angular.module('siteGenDirectives', []);
+
+// Directive: sgSearchForm
+    // sends user request to appropriate services after submission
+    // bonus: after submit, hide button and switch input to <a> with hover state; user can click to reenable input and search again
+
+// Directive: sgImage
+    // reads search term from scope
+    // takes target parameter "background" or "content"
+    // gets random image from photo/illustration API and either sets as background or injects <img> tag with src
+    // bonus: smart adaptation of image to screen size
+
+// Directive: sgIconHeader
+    // reads search term from scope
+    // gets random icon from nounProject API and sets as background on :before element
+
+// Directive: sgLipsum
+    // takes parameter of lipsum type (e.g. basic, hipster, jurassic)
+    // retrieves lipsum from appropriate API, or defaults to basic if not recognized
+    // prints out lipsum with html formatting
